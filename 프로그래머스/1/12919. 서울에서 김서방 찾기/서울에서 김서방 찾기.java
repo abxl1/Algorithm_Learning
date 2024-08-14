ff5b139fefda497ade89fbb537bcccc907211339
abxl <132882948+abxl1@users.noreply.github.com>
2024-08-14 09:57:09 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.66 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,15 +1,12 @@
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
-        // seoul 배열을 돌면서
-        for (int i = 0; i < seoul.length ; i++) {
-            // "Kim" 스트링과 일치 검사
-            if (seoul[i].equals("Kim")) {
-                // 일치한다면 위치한 인덱스 i 출력
+        for (int i = 0; i < seoul.length ; i++) { // seoul 배열을 돌면서
+            if (seoul[i].equals("Kim")) { // "Kim" 스트링과 일치 검사
+            // 인덱스를 포함한 문장 "김서방은 "+i+"에 있다" 저장
                 answer = "김서방은 "+i+"에 있다";
             }
-        }        
-        // 인덱스를 포함한 문장 return "김서방은 "+i+"에 있다" 출력
+        }
         return answer;
     }
 }
