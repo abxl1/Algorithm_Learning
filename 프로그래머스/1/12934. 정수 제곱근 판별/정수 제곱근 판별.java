0c75c615f1ea24f5f9efbf7482204df94dd002b7
abxl1 <132882948+abxl1@users.noreply.github.com>
2024-09-16 01:20:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.09 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,12 +1,8 @@
 class Solution {
     public long solution(long n) {
-        long result = 0; // long 결과값
-        double x = Math.sqrt(n); // double x는 n의 제곱근
-        if (x % 1 == 0) { // x가 정수라면
-            result = (long) Math.pow((x+1), 2); // x+1을 제곱
-        } else {
-            result = -1; // 정수가 아닌 실수라면 -1
-        }
-        return result;
+        double x = Math.sqrt(n);
+        return Math.sqrt(n) % 1 == 0 
+            ? (long) Math.pow((Math.sqrt(n) + 1), 2) 
+            : -1;
     }
 }
