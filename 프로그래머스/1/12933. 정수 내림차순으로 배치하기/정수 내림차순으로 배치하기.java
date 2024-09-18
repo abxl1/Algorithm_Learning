53f719bd74f3b458d9e4096638becda624b38721
abxl1 <132882948+abxl1@users.noreply.github.com>
2024-09-19 00:43:07 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.77 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 class Solution {
     public long solution(long number) {
         // 각 자리 숫자를 저장할 배열 초기화
-        String numberStr = Long.toString(number);
-        int length = numberStr.length();
+        String numStr = Long.toString(number);
+        int length = numStr.length();
         Integer[] digits = new Integer[length];
 
         // 숫자를 자릿수로 분리하여 배열에 저장
         for (int i = 0; i < length; i++) {
-            digits[i] = numberStr.charAt(i) - '0';
+            digits[i] = numStr.charAt(i) - '0';
         }
 
         // 배열을 내림차순으로 정렬
@@ -20,7 +20,6 @@ class Solution {
         for (int i = 0; i < length; i++) {
             reversedNumber = reversedNumber * 10 + digits[i];
         }
-
         return reversedNumber;
     }
 }
